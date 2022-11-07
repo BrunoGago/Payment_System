2206cbf9ba252b198ab7ab981141c31fee50368c
Bruno Gago <92176548+BrunoGago@users.noreply.github.com>
2022-11-07 10:58:04 -0300

    Date setting and class instantiation (Contract)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,32 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Contract;
 
 public class Program {
 
 	public static void main(String[] args){
+		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
-				
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
 		System.out.println("Entre com os dados do contrato");
 		System.out.println("------------------------------");
 		
 		System.out.print("Número: ");
-		
+		int number = sc.nextInt();
 		System.out.print("Data (dd/MM/yyyy HH:mm): ");
-		
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
 		System.out.print("Entre com o número de parcelas: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(number, date, totalValue);
 
 		
 		sc.close();
